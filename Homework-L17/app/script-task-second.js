4e699bb3d9a33db9a4f7377c9579e71df78da8dd
Viktor Gubarevich <gubarew.v@gmail.com>
2020-11-15 23:47:54 +0300

    Changed script-task-second.js

--- Homework-L17/app/script-task-second.js
+++ Homework-L17/app/script-task-second.js
@@ -1,22 +1,38 @@
 //Параметры дома пользователя.
 
-const floor = prompt('Please, enter the number of floors.', '');
-const entrance = prompt('Please, enter the number of entrance.', '');
-const stairCase = prompt('Please, enter the number of flat per floor.', '');
-const apartment = prompt('Please, enter apartment number.', '');
+const floor = Number(prompt('Please, enter the number of floors.', ''));
+const entrance = Number(prompt('Please, enter the number of entrance.', ''));
+const stairCaseFlat = Number(prompt('Please, enter the number of flat per floor.', ''));
+const apartment = Number(prompt('Please, enter apartment number.', ''));
 
-/*
-Вычисляем: 
-1. количество квартир в доме пользователя; 
-2. количество квартир в одном подъезде;
-3. в каком подъезде находится введенная квартира.
+const home = [floor, entrance, stairCaseFlat, apartment];
+
+// Вычисляем количество квартир в доме пользователя.
+const sumFlats = floor * entrance * stairCaseFlat;
+
+// Проверяем верно ли введены данные, заданные по условию задачи.
+
+if (home[0] > 25) {
+    alert('Mistake. Enter the number of floors from 1 to 25.');
+} else if (home[1] > 10) {
+    alert('Mistake. Enter the number of entrance from 1 to 10.');
+} else if (home[2] > 20) {
+    alert('Mistake. Enter the number of flat per floor from 1 to 20.');
+} else if (home[3] > 5000) {
+    alert('Mistake. Enter apartment number from 1 to 5000.');
+} else {
+    alert(`Amount of apartments in your house: ${sumFlats}.`);
+}
+
+/* Вычисляем:
+1. количество квартир в одном подъезде;
+2. в каком подъезде находится введенная квартира;
+3. на каком этаже находится введенная квартира.
 */
 
-const sumFlats = floor * entrance * stairCase; //1.
-alert(`Amount of apartments in your house: ${sumFlats}.`);
-
-const sumFlatsEntrance = floor * stairCase; //2.
-const numEntrance = Math.ceil(apartment / sumFlatsEntrance); //3.
+const sumFlatsEntrance = floor * stairCaseFlat; //1.
+const numEntrance = Math.ceil(apartment / sumFlatsEntrance); //2.
+const numFlatsEntrance = Math.ceil((apartment % sumFlatsEntrance) / stairCaseFlat); //3.
 
 
-// console.log(`Apartment number ${153} is located in the ${4}th entrance on the ${9}th floor.`);
+alert(`Apartment number ${apartment} is located in the ${numEntrance}th entrance on the ${numFlatsEntrance}th floor.`);
