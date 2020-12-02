b777b347272ddbe531fc489b618f243e07464156
Viktor Gubarevich <gubarew.v@gmail.com>
2020-12-02 22:37:40 +0300

    Homework/Classwork-L22

---
+++ Homework:Classwork-L22/app/script.js
@@ -0,0 +1,45 @@
+const addList = document.getElementById('addList');
+const addElementList = document.getElementById('addElementList');
+const removeLastElement = document.getElementById('removeLastElement');
+
+let ul;
+let li;
+
+addList.addEventListener('click', (event) => {
+
+    if (ul) {
+        alert('Список уже создан.')
+    } else {
+        ul = document.createElement('ul');
+        document.body.appendChild(ul);
+        ul.textContent = 'Список элементов:'
+    }
+
+});
+
+addElementList.addEventListener('click', (event) => {
+
+    if (!ul) {
+        alert('Список не создан. Создать элемент нельзя.')
+    } else {
+        const link = document.querySelector('ul');
+        li = document.createElement('li');
+        document.body.appendChild(li);
+        li.innerHTML = new Date().toLocaleString();
+    }
+
+
+});
+
+removeLastElement.addEventListener('click', (event) => {
+
+    const remove = document.getElementsByTagName('li');
+
+    if (remove.length === 0) {
+        alert('Удалить элемент невозможно т.к. его не существует.');
+        return;
+    }
+
+    const lastElement = remove[remove.length - 1];
+    lastElement.parentElement.removeChild(lastElement);
+});
