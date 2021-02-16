bee79ac313858f89a5a10a7b3fc0a4ae99e4937b
Viktor Gubarevich <gubarew.v@gmail.com>
2021-02-16 15:32:24 +0300

    feat: add project figma-land

--- Classwork-38(webpack)/webpack.config.js
+++ Classwork-38(webpack)/webpack.config.js
@@ -4,18 +4,23 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [{
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|otf|jpg|jpeg)$/,
+        use: 'url-loader?limit=100000'
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
     }),
   ],
-  module: {
-    rules: [{
-      test: /\.(scss|css)$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-    }, ],
-  },
 };
